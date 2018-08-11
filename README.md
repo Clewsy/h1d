4e5b9cc8e753ffa646a9a64a0b0dcdd07e15fa22
Clewsy <jadon.clews@gmail.com>
2018-08-11 21:08:45 +1000

    Additional comments and a readme.

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a straightforward implementation of Dean Camera' LUFA drivers starting with his HID keyboard demo.  The hardware is a Pololu A* (A-star) micro with an ATmega32U4.
+
+When connected via USB, the host will recognise the device as a standard keyboard.  A defined string will be "typed" a defined number of times and then... that's it!
--- The_String.h
+++ The_String.h
@@ -1,1 +1,3 @@
 #define THE_STRING	"The quick brown fox jumped over the lazy dog. 1234567890 !@#$%^&*()\n"
+
+//#define THE_STRING	"\nProgram memory\n\nProgram instructions are stored in non-volatile flash memory. Although the MCUs are 8-bit, each instruction takes one or two 16-bit words.\n\nThe size of the program memory is usually indicated in the naming of the device itself.\n\n"
